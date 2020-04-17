ae1cdd0645d08ef436ed1e1c7d5f7d46b88256ee
SuvaKo_ <pierre.bergerat@etu.unige.ch>
2020-04-17 14:06:50 +0200

    Affichage genres dans le catalogue

--- src/app/display-user-data/display-user-data.component.ts
+++ src/app/display-user-data/display-user-data.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { isBoolean } from 'util';
 
 @Component({
   selector: 'app-display-user-data',
@@ -8,12 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display-user-data.component.css']
 })
 export class DisplayUserDataComponent implements OnInit {
-  public j;
-  constructor(private router: Router, private httpClient: HttpClient) { }
+  public j: Array<String>;
+  public m: Array<Array<String>>;
+  constructor(private router: Router, private httpClient: HttpClient) { this.j = new Array<String>(); this.m = new Array<Array<String>>() }
   ngOnInit(): void {
     this.httpClient.get("http://localhost:3000/livres", { responseType: 'text' }).subscribe(res => {
       this.j = JSON.parse(res);
-      console.log(this.j);
+      var temp = JSON.parse(res);
+      for (var i = 0; i < this.j.length; i++) {
+        this.getGenre(temp[i].ID_Livre)
+      }
+      console.log(this.m)
+    })
+  }
+  getGenre(ID_Livre: String) {
+    var r = 'http://localhost:3000/liste/genres/' + ID_Livre
+    console.log(r)
+    this.httpClient.get(r, { responseType: 'json' }).subscribe(reponse => {
+      this.m.push(reponse as Array<String>)
     })
   }
 }
